ea70047c631f0b9722f1fc3d801e2a858bbe7be7
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-04-11 16:57:19 -0300

    Avances con el front end login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function Login() {
-  return (
-    <div className='bg-sky-900 h-screen flex flex-col items-center align-middle justify-center'>
-      <div className='bg-white h-2/3 w-1/3 flex align-middle justify-center rounded-xl'>
-        
-        <div className='flex flex-col items-center align-middle justify-center'>
+  const { register } = useForm()
 
-          <h1 className='text-3xl font-semibold'>¡Bienvenido de Regreso!</h1>
-          <form className='flex flex-col items-center align-middle justify-center'>
-            <input className='border border-gray-300 rounded-md w-3/3 h-10 my-2' type='text' placeholder='Usuario' />
-            <input className='border border-gray-300 rounded-md w-3/3 h-10 my-2' type='password' placeholder='Contraseña' />
-            <button className='bg-sky-900 text-white w-2/3 h-10 rounded-md my-2'>Inciar Sesión</button>
+  return (
+    <div className='bg-transparent sm:bg-sky-900 h-screen flex flex-col items-center align-middle justify-center'>
+      <div className='bg-white h-screen w-screen md:h-2/3 md:w-1/3 flex align-middle justify-center rounded-xl'>
+        <div className='w-screen flex flex-col items-center align-middle justify-center'>
+          <h1 className='open-sans text-3xl font-semibold'>¡Bienvenido!</h1>
+         <figure className='flex flex-col'>
+            <img className='w-28 sm:w-20' src="/Escudo_Policia_Chaco_Transparente.png" alt="" />
+          </figure>
+          <h1 className='open-sans text-xl'>Dpto. Violencia Familiar y de Género</h1>
+          
+          <form className='flex flex-col items-center align-middle justify-center w-4/5 sm:w-3/5' action="">
+            <input className='border open-sans border-gray-300 w-full rounded-md h-10 my-2' type='text' placeholder="Usuario"
+            {...register('username', {required: true } )} />
+            <input className='border open-sans border-gray-300 rounded-md w-full h-10 my-2' type='password' placeholder='Contraseña' />
+            <span>¿Has olvidado la contraseña? </span> <a href='/recover' className='text-sky-900'>Recuperar</a>
+            <button className='bg-sky-900 hover:bg-sky-700 text-white w-full h-10 rounded-md my-2'>Iniciar Sesión</button>
             <span className='text-sm'>¿No tienes cuenta? <a href='/register' className='text-sky-900'>Regístrate</a></span>
           </form>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
 
