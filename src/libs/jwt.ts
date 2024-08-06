cf731457fb5266cab34f8e2f83181c5cf3363424
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-08-06 19:52:10 -0300

    Por qué es tan difícil el deploy???

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -3,20 +3,24 @@ import jwt from 'jsonwebtoken'
 // Importar la clave secreta desde el archivo config.ts
 import { TOKEN_SECRET } from '../config'
 // Crear una función que genere un token de acceso
-export function createAccessToken (payload){   
-// Retorna una promesa que resuelve un token de acceso
+export function createAccessToken(payload) {
+    // Retorna una promesa que resuelve un token de acceso
     return new Promise((resolve, reject) => {
         // Firma un token con el payload y la clave secreta
         jwt.sign(
-            payload, TOKEN_SECRET, 
+            payload, TOKEN_SECRET,
             {
                 // En cuanto expira el token
+                domain: '.gonzaloebel.tech',
+                secure: process.env.NODE_ENV === 'production',
+                httpOnly: true,
+                sameSite: 'none', // Permite el envío entre sitios
                 expiresIn: 60 * 60 * 24
             }, (err, token) => {
-            if(err) reject(err)
-            resolve(token)
+                if (err) reject(err)
+                resolve(token)
             })
 
     })
-   
+
 }
