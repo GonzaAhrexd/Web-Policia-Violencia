971e1e462744420a816b7b8142ed415f9cd814db
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-08-07 07:56:33 -0300

    Por qué es tan difícil el deploy???

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 const baseUrl = import.meta.env.VITE_BASE_URL
+axios.defaults.withCredentials = true;
 
 const instance = axios.create({
+    
     baseURL: baseUrl,
     withCredentials: true
 })
