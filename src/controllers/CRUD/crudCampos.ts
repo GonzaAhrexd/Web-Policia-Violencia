a40d2db1010cafac1baca160fe845b214052a0ee
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-09-10 12:47:18 -0300

    Ediciones de campos

--- src/controllers/CRUD/crudCampos.ts
+++ src/controllers/CRUD/crudCampos.ts
@@ -21,3 +21,22 @@ export const obtenerCampo = async (req, res) => {
     }   
 
 }
+
+export const editarCampo = async (req, res) => {
+    try{
+        const { _id, campo, value } = req.body
+        const updatedCampo = await campos.findByIdAndUpdate(_id, {nombre: campo, value}, {new: true})
+        res.status(200).json(updatedCampo)
+    }catch(error){
+        console.log(error)
+    }
+}
+export const eliminarCampo = async (req, res) => {
+    try{
+        const { id } = req.params
+        await campos.findByIdAndDelete(id)
+        res.status(200).json('Campo eliminado')
+    }catch(error){
+        console.log(error)
+    }
+}
--- src/routes/campos.routes.ts
+++ src/routes/campos.routes.ts
@@ -1,11 +1,13 @@
 // Importamos Router desde express
 import { Router } from 'express';
-import { createCampo, obtenerCampo } from '../controllers/CRUD/crudCampos';
+import { createCampo, obtenerCampo, editarCampo, eliminarCampo } from '../controllers/CRUD/crudCampos';
 import { authAdmin } from '../middlewares/validateToken';
 const router:Router = Router();
 
 // Crear campo
 router.post('/agregar-campo/', authAdmin, createCampo)
 router.get('/obtener-campo/:tipo', authAdmin, obtenerCampo)
+router.put('/editar-campo/:id', authAdmin, editarCampo)    
+router.delete('/eliminar-campo/:id', authAdmin, eliminarCampo)
 
 export default router;
