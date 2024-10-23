34b2e795082782d39073b7f48d2ab3bccc0eff64
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-10-23 09:54:04 -0300

    Doble BD según modo

--- src/db.ts
+++ src/db.ts
@@ -7,9 +7,13 @@ const user:string | undefined = process.env.user
 const pass:string | undefined = process.env.pass
 const dbName:string | undefined = process.env.dbName
 //URI de la base de datos
+let uri:string = "";
 
-// const uri:string = `mongodb+srv://${user}:${pass}@cluster0.uijihcv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-const uri:string = 'mongodb://localhost:27017/PoliciaViolenciaGeneroCRUD'
+if(process.env.BD_mode == "cloud"){
+     uri = `mongodb+srv://${user}:${pass}@cluster0.uijihcv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+}else {
+     uri = 'mongodb://localhost:27017/PoliciaViolenciaGeneroCRUD'
+}
 
 //Función que realiza la conexión  a la base de datos de MongoDB
 export const connectDB = async (): Promise<void> => {
